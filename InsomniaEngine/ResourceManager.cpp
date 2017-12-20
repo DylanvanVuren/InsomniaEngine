b14391120e219a234abdd88c61326bdbadd65e21
14022370 <sasharadkova@gmail.com>
2017-12-20 13:30:04 +0100

    filled in more ResourceManager methods, but there;s a possible problem with ResourceManager::GetInstance()

--- InsomniaEngine/ResourceManager.cpp
+++ InsomniaEngine/ResourceManager.cpp
@@ -18,11 +18,41 @@ void ResourceManager::LoadTexture(IDirect3DDevice9* device, char* fileName)
 	Texture* texture = new Texture();
 
 	LPCTSTR fileNameLPCTSRT = CA2W(fileName); //convert a char array to pointer with the ATL conversion helper
-	if (!texture->Initialize(device, fileNameLPCTSRT))
+	if (!texture->Initialize(device, fileNameLPCTSRT)) //if it doesn't Initialize, send a message
 	{
 		delete texture;
-		std::cout << "Could not Initialize texture" << fileName << std::endl;
+		std::cout << "Could not Initialize texture " << fileName << std::endl;
 		return;
 	}
+	//else add it to the list of textures
+	m_textureList.push_back(texture);
+	std::cout << "Successfully loaded the texture " << fileName << std::endl;
+}
+
+LPDIRECT3DTEXTURE9 ResourceManager::GetTextureByName(char* textureName)
+{
+	for (int i=0; i<(int)m_textureList.size(); i++) //loop through the list of textures
+	{
+		Texture* texture = m_textureList[i];
+		std::string l_textureName = texture->GetName(); //get a local texture name
+
+		if (!strcmp(textureName, l_textureName.c_str())) //compare each l_textureName with the input textureName
+		{
+			return texture->GetTexture();
+		}
+	}
+
+	return NULL;
+}
+
+ResourceManager* ResourceManager::GetInstance()
+{
+	if(m_instance.m_pInstance == NULL )  //m_instance.m_pInstance because a nonstatic member reference 
+										//must be relative to a specific object
+	{
+		m_instance = ResourceManager(); //stack variable, is there even an instance? CHECK THIS
+	}
+
+	return m_instance.m_pInstance;
 
 }
--- InsomniaEngine/ResourceManager.h
+++ InsomniaEngine/ResourceManager.h
@@ -12,15 +12,16 @@ public:
 	~ResourceManager(void);
 
 	void LoadTexture(IDirect3DDevice9* device, char* fileName);
-	Texture* GetTextureByName(char* TextureName);
+	LPDIRECT3DTEXTURE9 GetTextureByName(char* TextureName);
 	static ResourceManager* GetInstance();
 
 private:
 	ResourceManager(void);//constructor is private because we only need one of these
 	std::vector<Texture*> m_textureList; //a resource manager must have access to multiple textures
 
-	static ResourceManager m_instance;
 
+	static ResourceManager m_instance;
+	ResourceManager* m_pInstance = &m_instance; //making a pointer for this instance
 };
 
 #endif
